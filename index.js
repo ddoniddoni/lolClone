11cce499b576f8c31cde995173b4b6ce59e28b61
ddoniddoni <psdkei@naver.com>
2020-04-10 17:42:36 +0900

    add image

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const btnMedia = document.getElementById("btn")
+const frontImage = "https://opgg-static.akamaized.net/images/lol/champion/";
+const backImage = ".png?image=q_auto,w_140&v=1585730185";
+
+const text = {
+  Garen: "Garen",
+  Galio: "Galio",
+};
+
+const image = document.getElementById("myImage");
+
+image.setAttribute("src", frontImage + text.Garen + backImage);
+image.setAttribute("width", "100");
+image.setAttribute("height", "100");
